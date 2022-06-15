e1021eba27536aed9c013850d8e14bbca8362796
hamza ezzakri <hamzaezzakri98@gmail.com>
2022-06-15 17:42:48 +0000

    setting up the project structure

---
+++ src/main/java/ma/cigma/pfe/MtisteApplication.java
@@ -0,0 +1,13 @@
+package ma.cigma.pfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtisteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtisteApplication.class, args);
+	}
+
+}
